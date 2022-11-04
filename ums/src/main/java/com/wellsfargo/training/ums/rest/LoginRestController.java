461f04a5f2209c0fbc318441a2f621f055ab1f7d
Akshay M <58382298+theakshaym@users.noreply.github.com>
2022-11-04 17:06:55 +0530

    Handle branch input

--- ums/src/main/java/com/wellsfargo/training/ums/rest/LoginRestController.java
+++ ums/src/main/java/com/wellsfargo/training/ums/rest/LoginRestController.java
@@ -17,51 +17,52 @@ import com.wellsfargo.training.ums.service.LoginRestService;
 //add @CrossOrigin for react here
 @RequestMapping(value = "/api")
 public class LoginRestController {
-	
+
 	@Autowired
 	private LoginRestService lrservice;
-	
+
 	//Open Postman and make POST request - http://localhost:8080/ums/api/register
-    //Under body tab --> raw --> Text --> Json and type the json data to be saved
+	//Under body tab --> raw --> Text --> Json and type the json data to be saved
 	@PostMapping("/register") 
-    public User createUser(@Validated @RequestBody User user) throws ResourceNotFoundException, UserAlreadyExistsException{
- 
-        try {
-	        System.out.println("Hello "+ user.getContactNumber()+" " +user.getPassword());
-	        User u=new User();
-	        
-	        u.setContactNumber(user.getContactNumber());
-	        u.setFirstName(user.getFirstName());
-	        u.setLastName(user.getLastName());
-	        u.setCity(user.getCity());
-	        u.setOccupation(user.getOccupation());
-	        u.setPassword(user.getPassword());
-	        System.out.println("Hello "+ user.getContactNumber()+" " +user.getPassword());
-	        u.setDob(user.getDob());
-	        u = lrservice.registerUser(u);
-	        return user;
-        }catch(DataIntegrityViolationException ex) {
-        	throw new UserAlreadyExistsException(user.getContactNumber());
-        }
-    }
-	
-	
-	 @PostMapping("/login")
-	    public Boolean loginDealer(@Validated @RequestBody User user) throws ResourceNotFoundException
-	    {
-	        Boolean a=false;
-	        Long contactNumber=user.getContactNumber();
-	        String password=user.getPassword();
-	        //System.out.println(email+password);
-	        User u = lrservice.loginUser(contactNumber).orElseThrow(() ->
-	        												new ResourceNotFoundException("User not found for this contact number: " + contactNumber + "."));
+	public User createUser(@Validated @RequestBody User user) throws ResourceNotFoundException, UserAlreadyExistsException{
 
-	        if(contactNumber.equals(u.getContactNumber()) && password.equals(u.getPassword()))
-	                {
-	            a=true;
+		try {
+			System.out.println("Hello "+ user.getContactNumber()+" " +user.getPassword());
+			User u=new User();
 
-	                }
-	        return a;
-	    }
+			u.setContactNumber(user.getContactNumber());
+			u.setFirstName(user.getFirstName());
+			u.setLastName(user.getLastName());
+			u.setCity(user.getCity());
+			u.setOccupation(user.getOccupation());
+			u.setPassword(user.getPassword());
+			System.out.println("Hello "+ user.getContactNumber()+" " +user.getPassword());
+			u.setDob(user.getDob());
+			u.setBranch(user.getBranch());
+			u = lrservice.registerUser(u);
+			return user;
+		}catch(DataIntegrityViolationException ex) {
+			throw new UserAlreadyExistsException(user.getContactNumber());
+		}
+	}
+
+
+	@PostMapping("/login")
+	public Boolean loginDealer(@Validated @RequestBody User user) throws ResourceNotFoundException
+	{
+		Boolean a=false;
+		Long contactNumber=user.getContactNumber();
+		String password=user.getPassword();
+		//System.out.println(email+password);
+		User u = lrservice.loginUser(contactNumber).orElseThrow(() ->
+		new ResourceNotFoundException("User not found for this contact number: " + contactNumber + "."));
+
+		if(contactNumber.equals(u.getContactNumber()) && password.equals(u.getPassword()))
+		{
+			a=true;
+
+		}
+		return a;
+	}
 
 }
