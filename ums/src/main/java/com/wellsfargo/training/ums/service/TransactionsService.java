37d1ce78df424551af0b595e9d08e33dfc71b75c
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-06 12:21:03 +0530

    added Transaction Service

---
+++ ums/src/main/java/com/wellsfargo/training/ums/service/TransactionsService.java
@@ -0,0 +1,42 @@
+
+package com.wellsfargo.training.ums.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transaction;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wellsfargo.training.ums.model.Transactions;
+import com.wellsfargo.training.ums.repository.TransactionsRepository;
+import com.wellsfargo.training.ums.response.TransactionRequest;
+
+@Service
+@Transactional
+public class TransactionsService {
+	
+	@Autowired
+	private TransactionsRepository tRepository;
+	public TransactionsService() {
+		// TODO Auto-generated constructor stub
+	}public void SaveTransaction(Transactions transaction) {
+		tRepository.save(transaction);
+		
+	}
+	
+	public List<Transactions> findall(TransactionRequest t) {
+		// TODO Auto-generated method stub
+		
+		
+		return tRepository.findall(t.getC_id(),t.getT_type(),t.getfDate(),t.gettDate());
+	}
+	
+	
+	}
+
+
+
