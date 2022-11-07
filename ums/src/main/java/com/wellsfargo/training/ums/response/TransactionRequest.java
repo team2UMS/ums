85a19af38dd6405dc82dfb4d8bc147289bdb7077
Akshay M <58382298+theakshaym@users.noreply.github.com>
2022-11-07 20:27:28 +0530

    Refactored TransactionRequest Class
    camel casing

--- ums/src/main/java/com/wellsfargo/training/ums/response/TransactionRequest.java
+++ ums/src/main/java/com/wellsfargo/training/ums/response/TransactionRequest.java
@@ -7,36 +7,40 @@ import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSe
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class TransactionRequest {
+
+	private long customerId;
 	
-	private long c_id;
-	private String t_type;
+	private String transactionType;
+	
 	@JsonFormat(pattern="yyyy-MM-dd")
-	private Date fDate;
-	private Date tDate;
-	public long getC_id() {
-		return c_id;
-	}
-	public void setC_id(long c_id) {
-		this.c_id = c_id;
-	}
-	public String getT_type() {
-		return t_type;
-	}
-	public void setT_type(String t_type) {
-		this.t_type = t_type;
-	}
-	public Date getfDate() {
-		return fDate;
-	}
-	public void setfDate(Date fDate) {
-		this.fDate = fDate;
-	}
-	public Date gettDate() {
-		return tDate;
-	}
-	public void settDate(Date tDate) {
-		this.tDate = tDate;
-	}
+	private Date fromDate;
 	
+	private Date toDate;
+	
+	
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	public String getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	public Date getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+	public Date getToDate() {
+		return toDate;
+	}
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
 
 }
