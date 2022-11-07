4a7c5d293ec96a391eea62dfd2e5803495a18b8e
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-07 19:40:42 +0530

    added transactions repository

--- ums/src/main/java/com/wellsfargo/training/ums/repository/TransactionsRepository.java
+++ ums/src/main/java/com/wellsfargo/training/ums/repository/TransactionsRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import javax.transaction.Transaction;
 
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -21,10 +22,13 @@ public interface TransactionsRepository extends JpaRepository<Transactions, Long
 	
 	
 	
-//	@Query("select t from Transactions t where t.customerId=:c_id")
-//	public List<Transactions> findall(long c_id,String t_type,Date fDate,Date tDate);
+	@Query("select t from Transactions t where t.user.customerId=:c and t.transactionType=:s and t.transactionDate>=:fdate and t.transactionDate<=:tdate")
+	public List<Transactions> findall(long c,String s,Date fdate,Date tdate);
+	
+	
+	@Query("select t from Transactions t where t.user.customerId=:c  and t.transactionDate>=:fdate and t.transactionDate<=:tdate")
+	public List<Transactions> findalltd(long c, Date fdate, Date tdate);
 
- 
 
 }
 
