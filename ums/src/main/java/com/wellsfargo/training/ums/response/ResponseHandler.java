26994468427907ad0ebcf686f6e0db1d8ec9bbac
Akshay M <theakshaym@gmail.com>
2022-11-07 20:36:47 +0530

    Comment

--- ums/src/main/java/com/wellsfargo/training/ums/response/ResponseHandler.java
+++ ums/src/main/java/com/wellsfargo/training/ums/response/ResponseHandler.java
@@ -6,6 +6,13 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+/*
+ *  ResponseHandler class is used to send a custom response JSON to the front-end. 
+ *  Message here refers to any message/additional information that needs to be send
+ *  HttpStatus is the server's response code to the request made. Ex: 200 - OK
+ *  responseObject is the data that needs to be sent to front-end. could be any object. Pass null if no data
+ *  needs to be sent to the front-end
+ */
 public class ResponseHandler {
 	
 	    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
