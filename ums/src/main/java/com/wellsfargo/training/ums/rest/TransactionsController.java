f6711524dcd0cecd58d69b7a6bc34c9543db70ff
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-07 19:46:22 +0530

    added transaction controller

--- ums/src/main/java/com/wellsfargo/training/ums/rest/TransactionsController.java
+++ ums/src/main/java/com/wellsfargo/training/ums/rest/TransactionsController.java
@@ -2,9 +2,12 @@
 
 package com.wellsfargo.training.ums.rest;
 
+import java.time.LocalDate;
 import java.util.*;
 
+import org.aspectj.weaver.ast.And;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wellsfargo.training.ums.model.Transactions;
 import com.wellsfargo.training.ums.response.TransactionRequest;
 import com.wellsfargo.training.ums.service.TransactionsService;
 
+import net.bytebuddy.asm.Advice.Return;
 import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 
@@ -37,24 +42,29 @@ public class TransactionsController {
 		t1.setTransactionDate(transaction.getTransactionDate());
 		//t1.setTransactionId(transaction.getTransactionId());
 		t1.setTransactionType(transaction.getTransactionType());
+		t1.setAmount(transaction.getAmount());
 		t1.setUser(transaction.getUser());
 	
 		tservice.SaveTransaction(t1);
 		return t1;
 		
+		
 	}
+	
 	@GetMapping("/statement")
 	public List<Transactions> ReadTransaction(@Validated @RequestBody TransactionRequest transaction){
 		
-		TransactionRequest t=new TransactionRequest();
+		if(transaction.getT_type().equals("withdraw")||transaction.getT_type().equals("deposit")) {
+			return tservice.findall(transaction);
+			
+			
+		}
+		else {
+			return tservice.findalltd(transaction);
 		
-		t.setC_id(transaction.getC_id());
-		t.setT_type(transaction.getT_type());
-		t.setfDate(transaction.getfDate());
-		t.settDate(transaction.gettDate());
-//		List<Transactions> x=tservice.findall(t);
-		List<Transactions> x=new ArrayList<>();
-		return x;
+		}
+		
+		
 	}
 
 }
