644780662a73137125599cb71c6839c79cdc3103
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-08 19:00:42 +0530

    Add files via upload

--- ums/src/main/java/com/wellsfargo/training/ums/service/LoanService.java
+++ ums/src/main/java/com/wellsfargo/training/ums/service/LoanService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wellsfargo.training.ums.model.Loan;
+import com.wellsfargo.training.ums.model.User;
 import com.wellsfargo.training.ums.repository.LoanRepository;
 
 import java.util.List;
@@ -23,12 +24,37 @@ public class LoanService {
 	}
 
 
-	public void saveLoan(Loan loan)
-	{
-		System.out.println("a");
-		lRepository.save(loan);
+
+	public void addnewcustomer(User u) {
+		// TODO Auto-generated method stub
+		Loan l=new Loan();
+		l.setCustomerId(u.getCustomerId());
+		l.setBranch(u.getBranch());
+		l.setLoanAmount(0);
+		lRepository.save(l);
 		
 	}
+
+
+
+	public void applyloan(Loan l) {
+		// TODO Auto-generated method stub
+		lRepository.applyloan(l.getCustomerId(),l.getLoanAmount());
+		
+		
+	}
+
+
+
+	public Loan findLoan(Loan l) {
+		// TODO Auto-generated method stub
+		return lRepository.findLoan(l.getCustomerId());
+	}
+
+
+
+	
+	
 	
 
 }
