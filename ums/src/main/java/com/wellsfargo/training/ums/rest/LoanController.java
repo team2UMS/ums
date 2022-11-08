70d1096341ed10d254700dafceb6942278500282
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-08 19:00:20 +0530

    Add files via upload

--- ums/src/main/java/com/wellsfargo/training/ums/rest/LoanController.java
+++ ums/src/main/java/com/wellsfargo/training/ums/rest/LoanController.java
@@ -1,5 +1,7 @@
 package com.wellsfargo.training.ums.rest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wellsfargo.training.ums.model.Loan;
+import com.wellsfargo.training.ums.response.ResponseHandler;
 import com.wellsfargo.training.ums.service.LoanService;
 
 import java.util.List;
@@ -20,27 +23,36 @@ public class LoanController {
 @Autowired
 private LoanService lservice;
 
-@PostMapping("/loan")
-public Loan addLoan(@Validated @RequestBody Loan loan)
+@PostMapping("/applyloan")
+public ResponseEntity<Object> addLoan(@Validated @RequestBody Loan loan)
 {
-	Loan l=new Loan();
-	System.out.println("a");
-	l.setBranch(loan.getBranch());
-	l.setCustomerId(loan.getCustomerId());
-	l.setLoanAmount(loan.getLoanAmount());
-	System.out.println("a");
-	lservice.saveLoan(loan);
 	
-	return l;
+	long lAmount=lservice.findLoan(loan).getLoanAmount()+loan.getLoanAmount();
+	
+	if(lAmount>1000000) {
+		return ResponseHandler.generateResponse("Loan limit(10,00,000) exceeded check your loan Balance once!", HttpStatus.OK, null);
+	}
+	else {
+		lservice.applyloan(loan);
+		return ResponseHandler.generateResponse("Success!", HttpStatus.OK, null);
+		
+	}
 }
 
-//@GetMapping("/statement")
-//public List<Loan> ReadLoan(@RequestParam("c_id") long x){
-//long cid=x;
-//List<Loan> l=lservice.findall(cid);
-//System.out.println(l);
-//System.out.println("d");
-//
-//return l;
-//}
+	
+
+	
+	@GetMapping("/loanbalance")
+	public ResponseEntity<Object> addLoan(@Validated @RequestParam(name="c_id") long customerId){
+		Loan l=new Loan();
+		l.setBranch("");
+		l.setCustomerId(customerId);
+		l.setLoanAmount(0);
+	
+		long lAmount=lservice.findLoan(l).getLoanAmount();
+		return ResponseHandler.generateResponse("Your loan amount is:"+lAmount, HttpStatus.OK, null);	
+	}
 }
+
+
+
