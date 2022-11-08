87ede3161f3f03b1dd738cede4cb1fcf3d3d7ddb
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-08 23:47:55 +0530

    Add files via upload

--- ums/src/main/java/com/wellsfargo/training/ums/response/ResponseHandler.java
+++ ums/src/main/java/com/wellsfargo/training/ums/response/ResponseHandler.java
@@ -24,4 +24,8 @@ public class ResponseHandler {
             return new ResponseEntity<Object>(map,status);
 	    }
 
+	
+
+		
+
 }
--- ums/src/main/java/com/wellsfargo/training/ums/response/TransactionObject.java
+++ ums/src/main/java/com/wellsfargo/training/ums/response/TransactionObject.java
@@ -2,14 +2,15 @@ package com.wellsfargo.training.ums.response;
 
 public class TransactionObject {
 	
-	private long c_id;
+	private long customerId;
 	private String type;
 	private long amount;
-	public long getC_id() {
-		return c_id;
+	
+	public long getCustomerId() {
+		return customerId;
 	}
-	public void setC_id(long c_id) {
-		this.c_id = c_id;
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
 	}
 	public String getType() {
 		return type;
