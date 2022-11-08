7ca23bab373b29cdaa8f1fda5080628fda3625f1
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-08 10:23:32 +0530

    added balance responce object

---
+++ ums/src/main/java/com/wellsfargo/training/ums/response/TransactionObject.java
@@ -0,0 +1,28 @@
+package com.wellsfargo.training.ums.response;
+
+public class TransactionObject {
+	
+	private long c_id;
+	private String type;
+	private long amount;
+	public long getC_id() {
+		return c_id;
+	}
+	public void setC_id(long c_id) {
+		this.c_id = c_id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	
+
+}
