52f63c28026c0eca59e741e3827a5cc815cf46f8
METTUKURUMADHAN <67286936+METTUKURUMADHAN@users.noreply.github.com>
2022-11-08 18:58:47 +0530

    Add files via upload

--- ums/src/main/java/com/wellsfargo/training/ums/repository/BalanceRepository.java
+++ ums/src/main/java/com/wellsfargo/training/ums/repository/BalanceRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.wellsfargo.training.ums.model.Balance;
+import com.wellsfargo.training.ums.model.Loan;
 
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
     @Modifying
-	@Query("update Balance b set b.aBalance=b.aBalance+:amount where b.customerId=:c_id")
+	@Query("update Balance b set b.accBalance=b.accBalance+:amount where b.customerId=:c_id")
 	void deposit(long c_id, long amount);
     @Modifying
-	@Query("update Balance b set b.aBalance=b.aBalance-:amount where b.customerId=:c_id")
-	void withdraw(long c_id, long amount);;
+	@Query("update Balance b set b.accBalance=b.accBalance-:amount where b.customerId=:c_id")
+	void withdraw(long c_id, long amount);
+    
+    
+    @Query("select b from Balance b where b.customerId=:customerId")
+	Balance balance(long customerId);
 }
